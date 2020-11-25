562649955cafff15e62b65fb5ba45dda146736cb
Silvana Cecilia Moreira <silvanamoreira85@outlook.com>
2020-11-25 12:26:33 +0100

    settings for deployment

--- server/app.js
+++ server/app.js
@@ -20,7 +20,9 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/project', require('./routes/project.routes'))
 app.use('/api/task', require('./routes/task.routes'))
 
+// Port
+const port = process.env.port || 5000
 // Server
-app.listen(process.env.PORT, '0.0.0.0', () => {
-    console.log(`Listening on http://localhost:${process.env.PORT}`)
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Listening on http://localhost:${port}`)
 })
