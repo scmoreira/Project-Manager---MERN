cf78a5302f2fd99f295531e42bf2b6cc3a10b9ec
scmoreira <silvanamoreira85@outlook.com>
2021-01-11 11:10:08 +0100

    checked

--- client/src/types/index.js
+++ client/src/types/index.js
@@ -1,5 +1,5 @@
 // Project types
-export const PROJECT_FORM = 'PROJECT_PROJECT'
+export const PROJECT_FORM = 'PROJECT_FORM'
 export const GET_PROJECTS = 'GET_PROJECTS'
 export const ADD_PROJECT = 'ADD_PROJECT'
 export const PROJECT_VALIDATION = 'PROJECT_VALIDATION'
